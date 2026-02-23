f3106703527c249b70153c52b59e1ddfe04b303e
Jhonatan Resende <jhonatanlira26@gmail.com>
2026-02-23 20:11:07 -0300

    Initial commit

---
+++ Verificador de Idade em C#/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace Verificador_de_Idade
+{
+    public class Person
+    {
+        public int Age;
+
+        public string IfLegalPersonRefactor(int Age)
+        {
+           string result = string .Empty;
+
+            if (Age >= 18)
+            {
+                result = "A pessoa é maior de idade!";
+                return result;
+			}
+            else
+            {
+                result = "A pessoa é menor de idade!";
+                return result; 
+			}
+		}
+	}
+}
---
+++ Verificador de Idade em C#/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Verificador_de_Idade;
+
+public class Program
+{
+	public static void Main()
+	{
+		Person person = new Person();
+
+		Console.WriteLine("Digite uma idade: ");
+
+		try
+		{
+			int Age = Convert.ToInt32(Console.ReadLine());
+
+			string response = person.IfLegalPersonRefactor(Age);
+			Console.WriteLine(response);
+		}
+		catch
+		{
+			Console.WriteLine("Valor inválido. Digite um valor válido.");
+		}
+	}
+}
